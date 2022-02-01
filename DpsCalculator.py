2422e4e2defa2c697dce013a6b32ede6aa6667e7
samuel <846433953@qq.com>
2022-02-01 16:31:26 -0500

    test1

---
+++ DpsCalculator.py
@@ -0,0 +1,16 @@
+# 输出期望计算器
+atk = int(input('攻击力?'))
+critR = int(input('暴击率？'))
+critD = int(input('暴伤？'))
+eleM = int(input('精通？'))
+eleD = int(input('元素伤害加成？'))
+addD = int(input('其他增伤乘区'))
+defD = int(input('减防？'))
+
+bare =
+reaction1 =
+reaction2 =
+
+print('无反应期望')
+print('1.5倍反应期望')
+print('2.0倍反应期望')
